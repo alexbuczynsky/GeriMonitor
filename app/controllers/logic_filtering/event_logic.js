c62a68b062375636aa2a61c49b9b3a50829735f0
Alexander Buczynsky <a.buczynsky@gmail.com>
2018-08-11 15:53:52 -0500

    fix waiting no motion in above bed

--- app/controllers/logic_filtering/event_logic.js
+++ app/controllers/logic_filtering/event_logic.js
@@ -34,7 +34,7 @@ module.exports.start_listening = async function start_listening(){
     push_events.on("motion Pillow", () => {
         setTimeout(() => {
             const above_bed_motion = machine_states.states.filter(x => x.name == "above_bed_motion");
-            if(above_bed_motion.seconds_since_tripped < waitSittingNoMotion){
+            if(above_bed_motion.seconds_since_tripped > waitSittingNoMotion){
                 machine_states.setState("sitting_detected",   false);
                 machine_states.setState("alarm",              false);
                 machine_states.setState("person_in_room",     false);
