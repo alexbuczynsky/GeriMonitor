9d254c7fa98d5084071234d6fdd2ee18bb75d9f5
Alexander Buczynsky <a.buczynsky@gmail.com>
2018-08-11 16:27:30 -0500

    asdfjksd

--- app/controllers/logic_filtering/event_logic.js
+++ app/controllers/logic_filtering/event_logic.js
@@ -47,10 +47,10 @@ module.exports.start_listening = async function start_listening(){
     // ZONE 2
     push_events.on("motion Above Bed", () => {
         machine_states.setState("above_bed_motion",true);
-        if(!machine_states.getState("person_in_room")){
+        if(!machine_states.getState("person_in_room") || machine_states.getState("person_in_room") == false || machine_states.getState("person_in_room") == "false"){
             machine_states.setState("sitting_detected", true);
             setTimeout(() => {
-                if(!machine_states.getState("person_in_room")){
+                if(!machine_states.getState("person_in_room") || machine_states.getState("person_in_room") == false || machine_states.getState("person_in_room") == "false"){
                     machine_states.setState("alarm", true);
                 }
                 machine_states.setState("sitting_detected", false);
